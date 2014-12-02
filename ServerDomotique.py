f65ff9da0b7d5a0b8d05ccb41a4f9046c8a90756
MalcolmMielle <malcolm.mielle@gmail.com>
2014-12-02 03:13:49 +0100

    design change

--- ServerDomotique.py
+++ ServerDomotique.py
@@ -11,13 +11,40 @@ parser = argparse.ArgumentParser(description='Domotic server center')
 parser.add_argument('--debug', action='store_true', help='Show debug messages')
 args = parser.parse_args()
 
+'''
+Basic conventions : 
+	Object are to a name, an address
+	Message have to be an Object and an order
+
+'''
+
+class PriseArduino(object):
+	def __init__(self, nbOfPlug):
+		self.nbOfPlug=nbOfPlug
+		
+	#Return the formated order that the plug can understand
+	def write(self, plug, order):
+		return str(plug)+";"+str(order)
+	
+	
+
 
 class ServerDomotique(object):
 	def __init__(self):
-		self.objects=dict()
-		self._logger = logging.getLogger(__name__)
+		self.instance=list()
+		self.instance.append(NRFinstance())
 		
+	
+	def testing(self):
+		for instance in self.instance:
+			instance.testing()
+		
+	
+class NRFinstance(object):
+	def __init__(self):
+		self._logger = logging.getLogger(__name__)
 		self.flag=0
+		self.obj=list()
 		
 		#Nrf24
 		self.nrf = Nrf24(cePin=2,csnPin=3,channel=10,payload=8)
@@ -25,8 +52,16 @@ class ServerDomotique(object):
 		self.nrf.setRADDR("server_rec")
 		self.nrf.setTADDR("server_send")
 		
+	def write(self, order):
+		for obj in self.obj:
+			if isinstance(obj, PriseArduino):
+				self._logger.info("Working with a Prise Arduino")
+				order_new=obj.write(order)
+				self._logger.info("Seding the object through NRF")
+				self.nrf.send(map,(ord,order_new))
 		
-	def testingNRF(self):
+		
+	def testing(self):
 		while True:
 			if self.flag==0:
 				if not self.nrf.isSending():
@@ -58,5 +93,5 @@ if __name__ == "__main__":
 		print "Make sure that the SPI is enable by editing : /etc/modprobe.d/raspi-blacklist.conf"
 		sys.exit(1)
 	
-	app.testingNRF()
+	app.testing()
 	
