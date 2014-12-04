2c4637ea5ce153df0df17216c55e2b6652341e09
MalcolmMielle <malcolm.mielle@gmail.com>
2014-12-04 22:18:07 +0100

    modified:   unit/test_lamp.py to follow PEP8

--- unit/test_lamp.py
+++ unit/test_lamp.py
@@ -1,23 +1,23 @@
-import random
 import unittest
 from Objects.Lamp import Lamp
 
-#To launch the test : 
-#from the unit folder : python -m unittest discover -t ..
+# To launch the test :
+# from the unit folder : python -m unittest discover -t ..
+
 
 class RandomTest(unittest.TestCase):
 
 	def test_Lamp(self):
 		print 'test lamp'
 		"""Test le fonctionnement de la class 'Lamp'."""
-		lamp=Lamp(1)
+		lamp = Lamp(1)
 		self.assertEqual(str(1), lamp.id)
-		lamp.id=52
+		lamp.id = 52
 		# Verifie que 'elt' est dans 'liste'
 		self.assertEqual(str(52), lamp.id)
-		
+
 		self.assertIn('on', lamp.order)
 		self.assertEqual('52;on_arduino', lamp.format_order('on'))
-		
+
 		with self.assertRaises(Exception):
 			lamp.format_order('wrong_order')
