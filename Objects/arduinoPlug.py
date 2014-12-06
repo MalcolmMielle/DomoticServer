a48d2d1762d5490ad90f44fe15775c313ea77175
MalcolmMielle <malcolm.mielle@gmail.com>
2014-12-06 14:12:19 +0100

    Simple implementation to send on order to the first module of Lamp graphically

--- Objects/arduinoPlug.py
+++ Objects/arduinoPlug.py
@@ -16,25 +16,37 @@ def isExistingClass(demand):
 
 
 class arduinoPlug(object):
-	def __init__(self, nb_plug=None):
+	def __init__(self, nb_plug=None, object_list=list()):
 		self._logger = logging.getLogger(__name__)
 		# Contains all object associated with there address
 		# (e.g the place on the plug)
-		self.arduinoPlug = list()
-		for nb, ad in enumerate(range(nb_plug)):
-			print 'On plug number ' + str(nb)
-			input = raw_input('What object : ')
-			classtype = isExistingClass(input)
+		try:
+			if nb_plug != len(object_list):
+				raise Exception("Nb of plugs and nb of object don't match")
+			if nb_plug < 1:
+				raise Exception("Should be more than 0 plugs")
+			self.arduinoPlug = object_list
+			if len(object_list) == 0:
+				for nb, ad in enumerate(range(nb_plug)):
+					print 'On plug number ' + str(nb)
+					input = raw_input('What object : ')
+					classtype = isExistingClass(input)
 
-			while classtype is False:
-				print 'Wrong input'
-				print 'On plug number ' + str(nb)
-				input = raw_input('What object : ')
-				classtype = isExistingClass(input)
+					while classtype is False:
+						print 'Wrong input'
+						print 'On plug number ' + str(nb)
+						input = raw_input('What object : ')
+						classtype = isExistingClass(input)
 
-			obj = copy.copy(classtype)
-			obj.id = nb
-			self.arduinoPlug.append(obj)
+					obj = copy.copy(classtype)
+					obj.id = nb
+					self.arduinoPlug.append(obj)
+			else:
+				for obj in object_list:
+					if isExistingClass(obj) is False:
+						raise Exception("The object " + str(type(obj)) + " is unknown")
+		except Exception, e:
+			print str(e)
 		self._logger.info(self.arduinoPlug)
 
 		# TODO ucomment for the pi + pass it as an attribute ?
--- WindowMecanism.py
+++ WindowMecanism.py
@@ -1,23 +1,37 @@
 import sys
-from PyQt4 import QtGui
 import mainwindow
+import argparse
+import logging
+from PyQt4 import QtGui
+from Objects.arduinoPlug import arduinoPlug
+from Objects.Lamp import Lamp
+
+# Argument on startup
+parser = argparse.ArgumentParser(description='Domotic server center')
+parser.add_argument('--debug', action='store_true', help='Show debug messages')
+args = parser.parse_args()
 
 
 class MainWindow(QtGui.QMainWindow):
-	def __init__(self):
+	def __init__(self, serverDomotic):
 		super(MainWindow, self).__init__()
 		self.ui = mainwindow.Ui_Dialog()
 		self.ui.setupUi(self)
-		self.ui.pushButton.clicked.connect(self.ChangeNumberPlug)
 		self.num_plug = 132
+		self.server = serverDomotic
 		self.initUi()
 
 	def initUi(self):
 		# self.btn.move(20, 20)
 		# self.le = QtGui.QLineEdit(self)
+		# self.ui.pushButton.clicked.connect(self.ChangeNumberPlug)
+		self.ui.pushButton.clicked.connect(self.sendONorder)
 		self.le = QtGui.QLabel(str(self.num_plug), self)
 		self.le.move(200, 20)
 		self.setWindowTitle('Input dialog')
+
+	def sendONorder(self):
+		self.server.send_order(0, "on")
 
 	def ChangeNumberPlug(self):
 		num, ok = QtGui.QInputDialog.getInt(self, 'Input Dialog', 'Nb of plug:')
@@ -52,8 +66,27 @@ class MainWindow_old(QtGui.QMainWindow):
 
 
 def main():
-	app = QtGui.QApplication(sys.argv)
-	other = MainWindow()
+	print "Server Domotique started"
+
+	logging.basicConfig()
+	logger = logging.getLogger()
+
+	if args.debug:
+		logger.setLevel(logging.DEBUG)
+
+	try:
+		app = QtGui.QApplication(sys.argv)
+	except:
+		logger.exception("Error occured!", exc_info=True)
+		print "Make sure that the SPI is enable by editing : \
+		/etc/modprobe.d/raspi-blacklist.conf"
+	# List of object
+	obj = list()
+	obj.append(Lamp(0))
+	obj.append(Lamp(1))
+	# TODO replace this by a server_domotique object
+	arduplug = arduinoPlug(2, obj)
+	other = MainWindow(arduplug)
 	other.show()
 	sys.exit(app.exec_())
 
