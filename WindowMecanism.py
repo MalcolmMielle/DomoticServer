bf0fd79989a6de8bca7d94ff9e79267bbadb7d65
MalcolmMielle <malcolm.mielle@gmail.com>
2014-12-06 03:46:20 +0100

    New interface from Qtdesigner

--- WindowMecanism.py
+++ WindowMecanism.py
@@ -3,22 +3,20 @@ from PyQt4 import QtGui
 import mainwindow
 
 
-class MainWindow(QtGui.QWidget):
+class MainWindow(QtGui.QMainWindow):
 	def __init__(self):
 		super(MainWindow, self).__init__()
+		self.ui = mainwindow.Ui_Dialog()
+		self.ui.setupUi(self)
+		self.ui.pushButton.clicked.connect(self.ChangeNumberPlug)
 		self.num_plug = 132
 		self.initUi()
 
 	def initUi(self):
-		self.btn = QtGui.QPushButton('Add number of plug : ', self)
 		# self.btn.move(20, 20)
-		self.btn.clicked.connect(self.ChangeNumberPlug)
-
 		# self.le = QtGui.QLineEdit(self)
 		self.le = QtGui.QLabel(str(self.num_plug), self)
 		self.le.move(200, 20)
-
-		self.setGeometry(300, 300, 500, 150)
 		self.setWindowTitle('Input dialog')
 
 	def ChangeNumberPlug(self):
@@ -28,6 +26,29 @@ class MainWindow(QtGui.QWidget):
 			# update the value inside the box
 			self.le.setNum(self.num_plug)
 			print 'type : ' + str(type(num)) + 'and texte : ' + str(self.num_plug)
+
+	def accept(self):
+		pass
+
+	def reject(self):
+		pass
+
+
+class MainWindow_old(QtGui.QMainWindow):
+	def __init__(self):
+		super(MainWindow_old, self).__init__()
+		self.ui = mainwindow.Ui_Dialog()
+		self.ui.setupUi(self)
+		self.ui.pushButton.clicked.connect(self.advanceSlider)
+
+	def advanceSlider(self):
+		self.ui.progressBar.setValue(self.ui.progressBar.value() + 1)
+
+	def accept(self):
+		pass
+
+	def reject(self):
+		pass
 
 
 def main():
